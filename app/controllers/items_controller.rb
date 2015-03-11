12230e767624348fa9cc8f5c8e00bccf3cece3e7
jgu <jeffwgu@gmail.com>
2015-03-11 17:41:50 -0600

    created status, orderitem relationships. also added universal links to make navigation easier.'

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,5 +12,4 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
-
 end
---
+++ spec/factories/order_items.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :order_item do
+    order_id 1
+    item_id 1
+    quantity 3
+  end
+end
