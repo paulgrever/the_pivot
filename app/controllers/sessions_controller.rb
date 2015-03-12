e2d385816968a92a10ef56654e9ac23d05c37794
jgu <jeffwgu@gmail.com>
2015-03-12 10:25:06 -0600

    changes to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,15 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Welcome, #{user.full_name}."
+      flash[:success] = "Welcome, #{user.full_name}."
     else
-      flash[:notice] = 'Invalid login.'
+      flash[:danger] = 'Invalid login.'
     end
     redirect_to(:back)
   end
 
   def destroy
-    flash[:notice] = "You have successfully signed out."
+    flash[:success] = "You have successfully signed out."
     session.clear
     redirect_to(:back)
   end
