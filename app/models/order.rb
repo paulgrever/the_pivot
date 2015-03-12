c7868b9856a64ad188140666b3d182f1076636a0
Lori Culberson <bigskybandit@sbcglobal.net>
2015-03-11 18:12:01 -0600

    Orders time and date methods

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,16 @@
+require 'date'
+
 class Order < ActiveRecord::Base
-	validates :user_id, :status_id, presence: true
+	validates :user_id, :status_id, :created_at, presence: true
 	belongs_to :user
+
+	def date
+		# created_at = Order.find(params[:created_at])
+		created_at.strftime("%Y-%m-%d")
+	end
+
+	def time
+		created_at.strftime("%I:%M %p")
+	end
+
 end
