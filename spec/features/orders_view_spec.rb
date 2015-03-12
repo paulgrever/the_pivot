4cf57172d046f9854f0296e1963546ad07188f72
Lori Culberson <bigskybandit@sbcglobal.net>
2015-03-12 14:27:38 -0600

    Order details changed click here to Details

--- spec/features/orders_view_spec.rb
+++ spec/features/orders_view_spec.rb
@@ -18,12 +18,12 @@ describe "the default users order view", type: :feature do
   end
 
   it "displays links for each individual order if there are orders" do
-    expect(page).to have_content("click here")
+    expect(page).to have_content("Details")
   end
 
   it "shows each orders indivdual information" do
     within('table tr:nth-child(2)') do
-      click_link_or_button("click here")
+      click_link_or_button("Details")
     end
       expect(page).to have_content("Details for Order ID:")
   end
@@ -33,7 +33,7 @@ describe "the default users order view", type: :feature do
   end
 
   it "displays order date" do
-    click_link_or_button( 'click here')
+    click_link_or_button( 'Details')
     expect(page).to have_content("Date ordered")
   end
 
