2b183ceda0ae6a9538ff60c796c03532d6e02f46
jgu <jeffwgu@gmail.com>
2015-03-16 20:57:27 -0600

    fixed all hound not having to do with css

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   validates :full_name, presence: true
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
-  validates :display_name, length: { maximum: 32, minimum: 2}, presence: false
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates :display_name, length: { maximum: 32, minimum: 2 }, presence: false
   has_secure_password
   has_many :orders
   has_many :order_items, through: :orders
