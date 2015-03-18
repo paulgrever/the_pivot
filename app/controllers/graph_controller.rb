199ae444b7694210176fe82af110797393e0fbc3
jgu <jeffwgu@gmail.com>
2015-03-17 18:40:41 -0600

    fixed d3 axis

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -7,12 +7,11 @@ class GraphController < ApplicationController
     items = Item.all
     item_titles = Item.pluck(:title)
     item_revenues = items.map(&:revenue)
-    item_revenues, item_titles =  item_revenues.zip(item_titles).sort.transpose
+    item_revenues, item_titles = item_revenues.zip(item_titles).sort.transpose
     revenue_data[:item] = item_titles
-    revenue_data[:revenue] = item_revenues
+    revenue_data[:numbers] = item_revenues
     respond_to do |format|
       format.json {
-        #render :json => { item: ["fish", "shrimp", "chips", "clams", "oysters"], revenue: [20, 30, 40, 50, 100] }.to_json
         render json: revenue_data.to_json
       }
     end
