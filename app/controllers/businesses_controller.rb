54b2d476a230343cf6d9d51036d1db22f40f8e46
Kit <dev_kit@icloud.com>
2015-04-02 12:02:40 -0600

    styling business show page

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,6 +1,6 @@
 class BusinessesController < ApplicationController
   def show
     @business = Business.first
-    @items = [ "250x300", "500x300", "500x700", "500x200", "500x600" ]
+    @items = [ "250x300", "300x300", "200x400", "200x200", "200x300" ]
   end
 end
