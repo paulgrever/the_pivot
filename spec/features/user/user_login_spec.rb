1e31e8d9b9c37bb17d9545115c7741bffcefc6e0
Austin Wood <i@austinwood.me>
2015-04-06 17:35:22 -0600

    Add test for auth with Twitter

--- spec/features/user/user_login_spec.rb
+++ spec/features/user/user_login_spec.rb
@@ -1,7 +1,8 @@
 require "rails_helper"
+OmniAuth.config.test_mode = true
 
 RSpec.describe "the user authentication feature", type: :feature do
-  context "a valid user logs in" do
+  context "when given correct credentials" do
     before :each do
       @user = FactoryGirl.create(:user)
       visit login_path
@@ -26,5 +27,38 @@ RSpec.describe "the user authentication feature", type: :feature do
     fill_in("session_password", with: "not paul")
     click_button "Sign in"
     expect(page).to have_content("Invalid login.")
+  end
+
+  context "when used through Twitter" do
+    it "allows the user to log in" do
+      visit login_path
+      expect(page).to have_content("Sign in with Twitter")
+      user = OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+        'provider' => 'twitter',
+        'uid' => '123545',
+        'info' => {
+          'name' => 'mockuser',
+          'image' => 'mock_user_thumbnail_url'
+        },
+        'credentials' => {
+          'token' => 'mock_token',
+          'secret' => 'mock_secret'
+        }
+      })
+      within(".button_to") do
+        click_link_or_button "Twitter"
+      end
+      expect(page).to have_content("Welcome, #{user.info.name}.")
+    end
+
+    it "can handle authentication error" do
+      OmniAuth.config.mock_auth[:twitter] = :invalid_credentials
+      visit login_path
+      expect(page).to have_content("Sign in with Twitter")
+      within(".button_to") do
+        click_link_or_button "Twitter"
+      end
+      expect(page).to have_content("Authentication failed")
+    end
   end
 end
