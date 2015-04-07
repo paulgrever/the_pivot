55024114d47a06d85827f3a44bca151b3b6cd262
Austin Wood <i@austinwood.me>
2015-04-07 13:31:00 -0600

    Okay, so I guess we needed those views, but they are empty..., fixed broken tests

--- spec/features/user/user_signup_spec.rb
+++ spec/features/user/user_signup_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe "user" do
   end
 
   it "can sign up with a name, email, and password" do
-    within("#signUpModal") do
+    within(".navbar-nav") do
       click_link_or_button("Sign up")
+    end
+    within("#signUpModal") do
       fill_in("name", with: "Paul")
       fill_in("email", with: "test@example.com")
       fill_in("password", with: "test")
@@ -18,12 +20,14 @@ RSpec.describe "user" do
   end
 
   it "can't sign up with an invalid email" do
-    within("#signUpModal") do
+    within(".navbar-nav") do
       click_link_or_button("Sign up")
+    end
+    within("#signUpModal") do
       fill_in("name", with: "Paul")
       fill_in("email", with: "hi")
       fill_in("password", with: "test")
-      click_button("Sign up")
+      click_link_or_button("Sign up")
     end
     expect(User.count).to eq(0)
     expect(page).to have_content("Input is invalid, please try again")
@@ -31,8 +35,10 @@ RSpec.describe "user" do
   end
 
   it "can't sign up without a full name" do
-    within("#signUpModal") do
+    within(".navbar-nav") do
       click_link_or_button("Sign up")
+    end
+    within("#signUpModal") do
       fill_in("name", with: "")
       fill_in("email", with: "test@example.com")
       fill_in("password", with: "test")
@@ -44,8 +50,10 @@ RSpec.describe "user" do
   end
 
   it "can't sign up without a passowrd" do
-    within("#signUpModal") do
+    within(".navbar-nav") do
       click_link_or_button("Sign up")
+    end
+    within("#signUpModal") do
       fill_in("name", with: "Paul")
       fill_in("email", with: "test@example.com")
       fill_in("password", with: "")
