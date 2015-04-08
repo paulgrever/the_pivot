347c49618ad05ef04a6fa8640a041fbbf6c60b89
Austin Wood <i@austinwood.me>
2015-04-07 21:08:18 -0600

    Sign in using AJAX request (but no flash message yet)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       if user && user.authenticate(params[:session][:password])
         session[:user_id] = user.id
         flash[:success] = "Welcome, #{user.full_name}."
-        redirect_to items_path
+        redirect_to :back
       else
         flash[:danger] = "Invalid login."
         render(:new)
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
       if user
         session[:user_id] = user.id
         flash[:success] = "Welcome, #{user.full_name}."
-        redirect_to items_path
+        redirect_to :back
       else
         flash[:danger] = "Invalid login."
         render(:new)
