b774044d8373763deafb0a15c987a15820fe7e96
Austin Wood <i@austinwood.me>
2015-04-08 17:08:43 -0600

    Businesses no longer namespaced under User

--- spec/features/business/guest_user_spec.rb
+++ spec/features/business/guest_user_spec.rb
@@ -1,11 +1,8 @@
 require "rails_helper"
 
-describe " a guest" do
+describe "A guest" do
   it "can view see the description" do
-    user = FactoryGirl.create(:user)
     business = FactoryGirl.create(:business)
-    allow_any_instance_of(ApplicationController).to receive(:current_user).
-      and_return(user)
-    visit business_path(business.id)
+    visit show_business_path(business.slug)
   end
 end
--- spec/features/business/owner_spec.rb
+++ spec/features/business/owner_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe "business" do
+describe "Business" do
   context "owner" do
     it "can create an new business" do
       user = FactoryGirl.create(:user)
@@ -10,7 +10,9 @@ describe "business" do
       fill_in "Name", with: "new business name"
       fill_in "Description", with: "business discription"
       click_button "Create Business"
+      business = Business.first
       expect(Business.count).to eq 1
+      expect(business.status).to eq("pending")
     end
 
     it "can update title and description" do
@@ -18,7 +20,7 @@ describe "business" do
       business = FactoryGirl.create(:business, user: business_owner)
       allow_any_instance_of(ApplicationController).to receive(:current_user).
         and_return(business_owner)
-      visit edit_user_business_path(business.id)
+      visit edit_business_path(business.id)
       fill_in "Description", with: "new description"
       click_link_or_button "Update Business"
     end
@@ -31,7 +33,7 @@ describe "business" do
       business = FactoryGirl.create(:business, user: business_owner)
       allow_any_instance_of(ApplicationController).to receive(:current_user).
         and_return(user)
-      visit edit_user_business_path(business.id)
+      visit edit_business_path(business.id)
       expect(current_path).to eq(items_path)
     end
 
@@ -41,7 +43,7 @@ describe "business" do
       business = FactoryGirl.create(:business, user: business_owner)
       allow_any_instance_of(ApplicationController).to receive(:current_user).
         and_return(un_auth_business_owner)
-      visit edit_user_business_path(business.id)
+      visit edit_business_path(business.id)
       expect(current_path).to eq(items_path)
     end
   end
