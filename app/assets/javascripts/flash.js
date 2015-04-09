d7c4363904f28cbf236236c24870f9ab1c908ff0
Austin Wood <i@austinwood.me>
2015-04-09 10:07:50 -0600

    Fix hound issue

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,7 +1,8 @@
 //fadeout flash messages
-$(document).ready(function() {
-    setTimeout(hideFlashes, 6000);
-});
 var hideFlashes = function() {
   $(".alert-notice, .alert-success, .alert-danger").fadeOut(4000);
 };
+
+$(document).ready(function() {
+    setTimeout(hideFlashes, 6000);
+});
