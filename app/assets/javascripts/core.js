2c1cddd90cc6e21d9198a632ab626a5d9692bd72
Austin Wood <i@austinwood.me>
2015-04-09 10:17:20 -0600

    Modal should flash error when invalid sign in

--- app/assets/javascripts/core.js
+++ app/assets/javascripts/core.js
@@ -2,8 +2,5 @@ $(document).ready(function() {
   return $("#signInModal").on("ajax:success", function(e, data, status, xhr) {
     $(this).modal("toggle");
     return location.reload(xhr.responseText);
-  }).on("ajax:error", function(e, xhr, status, error) {
-    $(this).modal("toggle");
-    return location.reload("<p>" + error + "</p>");
   });
 });
