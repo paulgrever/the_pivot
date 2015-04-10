1fe829f20cba301e88f0ba191d8aa5bb839702bf
Austin Wood <i@austinwood.me>
2015-04-09 22:41:41 -0600

    Clean up item index page

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,14 @@
 class Item < ActiveRecord::Base
-  validates :description, :price, presence: true
-  validates :title, presence: true, uniqueness: {case_sensitive: false}
-  validates :price, numericality: { greater_than: 0}
+  validates :title, presence: true, uniqueness: { case_sensitive: false }
+  validates :price, presence: true, numericality: { greater_than: 0}
+  validates :description, presence: true
+
   belongs_to :item_status
   has_many :item_categories
+  has_many :categories, through: :item_categories
   has_many :order_items
   has_many :orders, through: :order_items
-  has_many :categories, through: :item_categories
+
   scope :active, -> { where(item_status_id: 1) }
   has_attached_file :image, styles: { large: "500x340>", medium: "250x170>", thumb: "100x100>" }, default_url: "supply_default.png"
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
