c8e6d03e02fa0a1fba585a69f80afc56a47f0f42
Austin Wood <i@austinwood.me>
2015-04-13 14:32:42 -0600

    Include custom css class for each item; change price of item factories

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -2,12 +2,12 @@ FactoryGirl.define do
   factory :item do
     title "Water bottle"
     description "What holy water contains a little drop, little drop for me?"
-    price 2
+    price 200
   end
 
   factory :item2, class: Item do
     title "Food package"
     description "pre-packaged, proteined-packed"
-    price 10
+    price 1099
   end
 end
