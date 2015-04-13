160190e9daaea899adc0d889513b52f60e880257
Kit <dev_kit@icloud.com>
2015-04-13 17:48:39 -0600

    remove business factory

---
+++ spec/factories/business.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :business do
+    name "Water merchant"
+    description "Water water everywhere and not a drop to drink."
+  end
+
+ factory :other_business, class: Business do
+    name "Blanket merchants"
+    description "Insert creative description here"
+  end
+end
