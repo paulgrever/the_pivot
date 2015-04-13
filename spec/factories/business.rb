ab899a9e2bc8b8a9e79caee1df0319a4c3e427ac
Kit <dev_kit@icloud.com>
2015-04-13 17:51:22 -0600

    factory businesses tests

--- spec/factories/business.rb
+++ spec/factories/business.rb
@@ -2,10 +2,12 @@ FactoryGirl.define do
   factory :business do
     name "Water merchant"
     description "Water water everywhere and not a drop to drink."
+    user
   end
 
  factory :other_business, class: Business do
     name "Blanket merchants"
     description "Insert creative description here"
+    user
   end
 end
