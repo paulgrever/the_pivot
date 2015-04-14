24a7b1aca07f7d728017e2dbe07132f136800e21
Austin Wood <i@austinwood.me>
2015-04-13 18:15:28 -0600

    Remove CanCanCan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :cart
   before_action :load_cart
 
-  rescue_from CanCan::AccessDenied do |exception|
-    flash[:danger] = exception.message
-    redirect_to root_path
-  end
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
