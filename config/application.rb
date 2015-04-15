16501e73c6c84bd05ba90469427bd8b7941e1b34
Austin Wood <i@austinwood.me>
2015-04-15 12:45:22 -0600

    Enable Skylight to work in development

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,8 @@ module DinnerDash
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Enable Skylight to work in development
+    config.skylight.environments += ["development"]
   end
 end
