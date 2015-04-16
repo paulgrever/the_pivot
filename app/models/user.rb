ccdd1994b124522b56309c27aab3e87bd347ac4b
Paul Grever <paulgrever@gmail.com>
2015-04-15 21:15:25 -0600

    set up previews

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,8 +40,6 @@ class User < ActiveRecord::Base
   end
 
   def checkout_ready?
-    if credit_card.present? && shipping_address.present? && billing_address.present?
-      true
-    end
+    credit_card.present? && shipping_address.present? && billing_address.present?
   end
 end
