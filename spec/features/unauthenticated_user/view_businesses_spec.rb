11b83f7ccd6e8233daf341f2710ceb153c52adda
Austin Wood <i@austinwood.me>
2015-04-15 18:40:51 -0600

    Add spec for viewing a business show when unauthenticated

--- spec/features/unauthenticated_user/view_businesses_spec.rb
+++ spec/features/unauthenticated_user/view_businesses_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe "Unauthenticated user", type: :feature do
       expect(page.status_code).to eq(404)
     end
   end
+
+  it "should be able to visit a business page" do
+    business_owner = create(:business_owner)
+    business = Business.create!(name: "New business", user: business_owner)
+    visit show_business_path(business.slug)
+    within(".jumbotron") do
+      expect(page).to have_content("New business")
+    end
+  end
 end
